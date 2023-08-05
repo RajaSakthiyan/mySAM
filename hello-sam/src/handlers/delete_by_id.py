8293252a427a53966f834847b70522ec323b675c
RajaSakthiyan <rajasakthiyan.g@gmail.com>
2023-08-05 18:57:02 +0530

    Basic apigw-lambda-dynamodb

---
+++ hello-sam/src/handlers/delete_by_id.py
@@ -0,0 +1,24 @@
+# Owner: Rajasakthiyan.G
+# Email: rajasakthiyan.g@gmail.com
+
+import json
+import os
+import boto3
+
+client = boto3.client('dynamodb')
+TABLE_NAME = os.environ["TABLE_NAME"]
+
+def delete_by_id_handler(event, context):
+    """
+    Lambda handler that performing delete call to DynamoDB.
+    The naem of the table is 'TABLE_NAME' from enviornment
+    """
+    id = event["pathParameters"]["id"]
+    data = client.get_item(TableName=TABLE_NAME, Key={"id": {"S": id}})
+    item = data["Item"]
+    response = {
+        "statusCode": 202,
+        "body": json.dumps(item)
+    }
+
+    return response
---
+++ hello-sam/src/handlers/put_item.py
@@ -0,0 +1,69 @@
+# Owner: Rajasakthiyan.G
+# Email: rajasakthiyan.g@gmail.com
+
+import json
+import os
+import boto3
+
+client = boto3.client("dynamodb")
+
+EXPECTED_KEYS = ("id", "Weather")
+TABLE_NAME = os.environ["TABLE_NAME"]
+
+
+class InvalidRequestError(Exception):
+    """
+    Custom exception for capturing unknown keys present in given request body.
+    """
+
+    def __str__(self) -> str:
+        return "'id' and 'Weather' are the only attributes in the request body, contains other attributes"
+
+
+class MissingKeyError(Exception):
+    """
+    Custom exception for capturing missing keys.
+    """
+
+    def __init__(self, missing_key: str, *args: object) -> None:
+        self.missing_key = missing_key
+        super().__init__(*args)
+
+    def __str__(self) -> str:
+        return f"'{self.missing_key}' is missing. 'id' and 'Weather' attributes should be present in the request body"
+
+
+def validate_expected_data(data):
+    """
+    Validate expected keys in EXPECTED_KEYS.
+    """
+    for key in EXPECTED_KEYS:
+        if not key in data:
+            raise MissingKeyError(key)
+
+
+def validate_valid_data(data):
+    """
+    Validate the data with number of keys in EXPECTED_KEYS.
+    """
+    if not len(data) == len(EXPECTED_KEYS):
+        raise InvalidRequestError()
+
+
+def put_item_handler(event, context):
+    """
+    Lambda handler that performing validation and put call to DynamoDB.
+    The naem of the table is 'TABLE_NAME' from enviornment
+    """
+    try:
+        body = json.loads(event["body"])
+        validate_expected_data(body)
+        validate_valid_data(body)
+        id, Weather = body["id"], body["Weather"]
+        result = client.put_item(
+            TableName=TABLE_NAME,
+            Item={"id": {"S": id}, "name": {"S": Weather}},
+        )
+        return {"statusCode": 200, "body": json.dumps(result)}
+    except [MissingKeyError, InvalidRequestError] as error:
+        return {"statusCode": 400, "body": {"error": str(error)}}
